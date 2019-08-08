fce7ba084ef8cc26107733c53e6d8a6f8df7748b
yusai13 <future.gazer7@gmail.com>
2019-08-08 23:28:08 +0900

    TOPページ作成、eventモデル作成

--- db/schema.rb
+++ db/schema.rb
@@ -10,7 +10,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 2019_08_06_045516) do
+ActiveRecord::Schema.define(version: 2019_08_06_173409) do
 
   create_table "active_admin_comments", force: :cascade do |t|
     t.string "namespace"
@@ -47,6 +47,8 @@ ActiveRecord::Schema.define(version: 2019_08_06_045516) do
     t.datetime "created_at", null: false
     t.datetime "updated_at", null: false
     t.boolean "admin", default: false, null: false
+    t.string "name", default: "", null: false
+    t.text "profile", default: ""
     t.index ["email"], name: "index_users_on_email", unique: true
     t.index ["reset_password_token"], name: "index_users_on_reset_password_token", unique: true
   end

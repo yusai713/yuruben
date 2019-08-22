be99b549315949203279fa41fd9dcbd0ac471de4
yusai13 <future.gazer7@gmail.com>
2019-08-22 20:01:44 +0900

    参加者ユーザー表示機能

--- app/controllers/events_controller.rb
+++ app/controllers/events_controller.rb
@@ -8,6 +8,8 @@ class EventsController < ApplicationController
 
   def index
     @events = Event.where("starts_at  > ?", DateTime.now).order(:starts_at)
+    #@binding.irb
+    @event_users= @events.joins(:user).group(:event_id).select('user_id,image')
   end
 
   def new
